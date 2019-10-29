fc148dea3fc63e4df4ca94e6f28f3fb60d79c19a
Victor Hermann Chiletto <victor.vasconceloschiletto@gmail.com>
2019-10-29 19:57:55 -0300

    Prettier

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
-import serve from 'rollup-plugin-serve';
-import livereload from 'rollup-plugin-livereload';
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
 
 const watch = !process.env.ROLLUP_WATCH;
 
@@ -10,17 +10,17 @@ const plugins = [
     resolve(),
     commonjs(),
     watch && terser(),
-    !watch && serve({ contentBase: './public', verbose: true }),
+    !watch && serve({ contentBase: "./public", verbose: true }),
     !watch && livereload(),
 ];
 
 export default [
     {
-        input: 'src/index.js',
+        input: "src/index.js",
         output: {
-            file: 'public/index.js',
-            format: 'esm',
-            sourcemap: true
+            file: "public/index.js",
+            format: "esm",
+            sourcemap: true,
         },
         plugins,
     },
